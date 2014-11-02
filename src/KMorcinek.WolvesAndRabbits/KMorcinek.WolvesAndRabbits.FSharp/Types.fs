e9bbc56e97e2fed363bcca6b9832842b38c95197
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2014-11-02 11:01:01 +0100

    new type Rabbit

--- src/KMorcinek.WolvesAndRabbits/KMorcinek.WolvesAndRabbits.FSharp/Types.fs
+++ src/KMorcinek.WolvesAndRabbits/KMorcinek.WolvesAndRabbits.FSharp/Types.fs
@@ -1,9 +1,10 @@
 ﻿namespace FsTypes
 
-type Rabbit =
-    struct 
-       val Position : int
-       val Food : double
-       new(position, food) = { Position = position; Food = food}
-    end
+type Rabbit(position:int, food:double) =
+    member this.Position = position
+    member this.Food = food
+//    let position = position
+//       val Position : int
+//       val Food : double
+//       new(position, food) = { Position = position; Food = food}
 
