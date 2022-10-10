e08dbb5bee30e723a0f2e6879f06852778c03bb7
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2022-10-10 16:11:18 +0200

    index

--- new-wolves/KMorcinek.WolvesAndRabbits.Web/Controllers/IndexController.cs
+++ new-wolves/KMorcinek.WolvesAndRabbits.Web/Controllers/IndexController.cs
@@ -3,17 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace KMorcinek.WolvesAndRabbits.Web.Controllers;
 
 [ApiController]
-[Route("[controller]")]
 public class IndexController : ControllerBase
 {
-    [HttpGet("verify")]
-    public ContentResult Verify()
-    {
-        var html = "<div>Your account has been verified.</div>";
-        return base.Content(html, "text/html");
-    }
-
-    [HttpGet("confirm-verify")]
+    [HttpGet("")]
     public ContentResult ConfirmVerify()
     {
         var html = System.IO.File.ReadAllText(@"./assets/verified.html");
