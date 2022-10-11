725aa6e6e4443a56005a4455c883cf448baeadbf
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2022-10-11 06:57:54 +0200

    useStaticFiles

--- new-wolves/KMorcinek.WolvesAndRabbits.Web/Controllers/IndexController.cs
+++ new-wolves/KMorcinek.WolvesAndRabbits.Web/Controllers/IndexController.cs
@@ -5,10 +5,17 @@ namespace KMorcinek.WolvesAndRabbits.Web.Controllers;
 [ApiController]
 public class IndexController : ControllerBase
 {
-    [HttpGet("")]
-    public ContentResult ConfirmVerify()
-    {
-        var html = System.IO.File.ReadAllText(@"./assets/index.html");
-        return base.Content(html, "text/html");
-    }
+    // [HttpGet("")]
+    // public ContentResult ConfirmVerify()
+    // {
+    //     var html = System.IO.File.ReadAllText(@"~/index.html");
+    //     return base.Content(html, "text/html");
+    // }
+    
+    // [HttpGet("main.js")]
+    // public ContentResult MainJs()
+    // {
+    //     var html = System.IO.File.ReadAllText(@"./assets/main.js");
+    //     return base.Content(html, "text/html");
+    // }
 }
