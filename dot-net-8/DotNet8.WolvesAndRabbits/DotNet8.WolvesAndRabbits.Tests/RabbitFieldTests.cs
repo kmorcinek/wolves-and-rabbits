ea1c1e8ad42253cd118c7e4c8973a252cf2b1326
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2024-05-18 09:24:49 +0200

    remove xuning by GPT

--- dot-net-8/DotNet8.WolvesAndRabbits/DotNet8.WolvesAndRabbits.Tests/RabbitFieldTests.cs
+++ dot-net-8/DotNet8.WolvesAndRabbits/DotNet8.WolvesAndRabbits.Tests/RabbitFieldTests.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using KMorcinek.WolvesAndRabbits.Configuration;
 using Xunit;
-using Xunit.Should;
 
 namespace KMorcinek.WolvesAndRabbits.Tests
 {
@@ -19,51 +18,50 @@ namespace KMorcinek.WolvesAndRabbits.Tests
         [Fact]
         public void OneRabbitEatsOneLettuce()
         {
-            List<Lettuce> lettuces = new List<Lettuce>(new[] { new Lettuce(0, 10) });
-
+            List<Lettuce> lettuces = new List<Lettuce> { new Lettuce(0, 10) };
             Rabbit rabbit = new Rabbit(0, 10);
 
             var fieldWithRabbit = rabbitField.PredatorMovesAndEatsOnlyBestPrey(lettuces, rabbit);
 
-            fieldWithRabbit.Item1.First().Food.ShouldBe(3);
-            fieldWithRabbit.Item2.Food.ShouldBe(17);
+            Assert.Equal(3, fieldWithRabbit.Item1.First().Food);
+            Assert.Equal(17, fieldWithRabbit.Item2.Food);
         }
 
         [Fact]
         public void GivenManyLettuces_RabbitChoosesBestLettuceAndEats()
         {
             Lettuce bestLettuce = new Lettuce(20, 15);
-            List<Lettuce> lettuces = new List<Lettuce>(new[]
+            List<Lettuce> lettuces = new List<Lettuce>
             {
                 bestLettuce,
                 new Lettuce(0, 10),
                 new Lettuce(-20, 12)
-            });
+            };
 
             Rabbit rabbit = new Rabbit(0, 10);
 
             var fieldWithRabbit = rabbitField.PredatorMovesAndEatsOnlyBestPrey(lettuces, rabbit);
 
-            fieldWithRabbit.Item1.Single(p => p.Position == bestLettuce.Position).Food.ShouldBe(8);
-            fieldWithRabbit.Item2.Food.ShouldBe(17);
+            Assert.Equal(8, fieldWithRabbit.Item1.Single(p => p.Position == bestLettuce.Position).Food);
+            Assert.Equal(17, fieldWithRabbit.Item2.Food);
         }
 
         [Fact]
         public void GivenManyLettuces_RabbitChoosesBestLettuceAndMoves()
         {
             Lettuce bestLettuce = new Lettuce(20, 15);
-            List<Lettuce> lettuces = new List<Lettuce>(new[]
+            List<Lettuce> lettuces = new List<Lettuce>
             {
                 new Lettuce(0, 10),
                 bestLettuce,
                 new Lettuce(-20, 12),
-            });
+            };
 
             Rabbit rabbit = new Rabbit(-1, 10);
 
             var fieldWithRabbit = rabbitField.PredatorMovesAndEatsOnlyBestPrey(lettuces, rabbit);
 
-            fieldWithRabbit.Item2.Position.ShouldBe(bestLettuce.Position);
+            Assert.Equal(bestLettuce.Position, fieldWithRabbit.Item2.Position);
         }
 
         [Fact]
@@ -71,146 +69,146 @@ namespace KMorcinek.WolvesAndRabbits.Tests
         {
             Lettuce bestLettuce = new Lettuce(20, 15);
             Lettuce notBestLettuce = new Lettuce(-20, 12);
-            List<Lettuce> lettuces = new List<Lettuce>(new[]
+            List<Lettuce> lettuces = new List<Lettuce>
             {
                 bestLettuce,
                 notBestLettuce
-            });
+            };
 
             Rabbit rabbit = new Rabbit(0, 10);
 
             var fieldWithRabbit = rabbitField.PredatorMovesAndEatsOnlyBestPrey(lettuces, rabbit);
-            fieldWithRabbit.Item1.Count().ShouldBe(2);
-            fieldWithRabbit.Item1.Single(p => p.Position == notBestLettuce.Position).Food.ShouldBe(notBestLettuce.Food);
+            Assert.Equal(2, fieldWithRabbit.Item1.Count());
+            Assert.Equal(notBestLettuce.Food, fieldWithRabbit.Item1.Single(p => p.Position == notBestLettuce.Position).Food);
         }
 
         [Fact]
         public void ManyRabbitsEatManyLettuces()
         {
-            List<Lettuce> lettuces = new List<Lettuce>(new[]
+            List<Lettuce> lettuces = new List<Lettuce>
             {
                 new Lettuce(20, 15),
                 new Lettuce(0, 10),
                 new Lettuce(-20, 12)
-            });
+            };
 
-            List<Rabbit> rabbits = new List<Rabbit>(new[]
+            List<Rabbit> rabbits = new List<Rabbit>
             {
                 new Rabbit(0, 10),
                 new Rabbit(0, 9),
-            });
+            };
 
             var fieldWithRabbit = rabbitField.GetNextTurn(lettuces, rabbits);
 
-            fieldWithRabbit.Item1.Single(p => p.Position == 20).Food.ShouldBe(8);
-            fieldWithRabbit.Item1.Single(p => p.Position == -20).Food.ShouldBe(5);
+            Assert.Equal(8, fieldWithRabbit.Item1.Single(p => p.Position == 20).Food);
+            Assert.Equal(5, fieldWithRabbit.Item1.Single(p => p.Position == -20).Food);
             List<Rabbit> nextRabbits = fieldWithRabbit.Item2.ToList();
-            nextRabbits.Count().ShouldBe(2);
-            nextRabbits[0].Food.ShouldBe(15);
-            nextRabbits[1].Food.ShouldBe(14);
+            Assert.Equal(2, nextRabbits.Count);
+            Assert.Equal(15, nextRabbits[0].Food);
+            Assert.Equal(14, nextRabbits[1].Food);
         }
 
         [Fact]
-        void OneRabbitChoosesBestLettuce()
+        public void OneRabbitChoosesBestLettuce()
         {
-            List<Lettuce> lettuces = new List<Lettuce>(new[]
+            List<Lettuce> lettuces = new List<Lettuce>
             {
                 new Lettuce(0, 10),
                 new Lettuce(-20, 12)
-            });
+            };
 
             Rabbit rabbit = new Rabbit(0, 10);
 
             Lettuce lettuce;
-            rabbitField.TryChooseBestPrey(lettuces, rabbit, out lettuce).ShouldBeTrue();
-            lettuce.Position.ShouldBe(-20);
+            Assert.True(rabbitField.TryChooseBestPrey(lettuces, rabbit, out lettuce));
+            Assert.Equal(-20, lettuce.Position);
         }
 
         [Fact]
-        void OneRabbitChoosesBestLettuce_2()
+        public void OneRabbitChoosesBestLettuce_2()
         {
-            List<Lettuce> lettuces = new List<Lettuce>(new[]
+            List<Lettuce> lettuces = new List<Lettuce>
             {
                 new Lettuce(20, 15),
                 new Lettuce(0, 10),
                 new Lettuce(-20, 12)
-            });
+            };
 
             Rabbit rabbit = new Rabbit(0, 10);
 
             Lettuce lettuce;
-            rabbitField.TryChooseBestPrey(lettuces, rabbit, out lettuce).ShouldBeTrue();
+            Assert.True(rabbitField.TryChooseBestPrey(lettuces, rabbit, out lettuce));
 
-            lettuce.Position.ShouldBe(20);
+            Assert.Equal(20, lettuce.Position);
         }
 
         [Fact]
-        void GivenNotEnoughLettuce_RabbitEatsEverything()
+        public void GivenNotEnoughLettuce_RabbitEatsEverything()
         {
-            List<Lettuce> lettuces = new List<Lettuce>(new[] { new Lettuce(0, 2) });
+            List<Lettuce> lettuces = new List<Lettuce> { new Lettuce(0, 2) };
 
             Rabbit rabbit = new Rabbit(0, 10);
 
             var fieldWithRabbit = rabbitField.PredatorMovesAndEatsOnlyBestPrey(lettuces, rabbit);
 
-            fieldWithRabbit.Item1.First().Food.ShouldBe(0);
+            Assert.Equal(0, fieldWithRabbit.Item1.First().Food);
         }
 
         [Fact]
-        void GivenNotEnoughLettuce_RabbitsFeedsWithPercentageOfEatenFood()
+        public void GivenNotEnoughLettuce_RabbitsFeedsWithPercentageOfEatenFood()
         {
-            List<Lettuce> lettuces = new List<Lettuce>(new[] { new Lettuce(0, 2) });
+            List<Lettuce> lettuces = new List<Lettuce> { new Lettuce(0, 2) };
 
             Rabbit rabbit = new Rabbit(0, 0);
 
             var fieldWithRabbit = rabbitField.PredatorMovesAndEatsOnlyBestPrey(lettuces, rabbit);
 
-            fieldWithRabbit.Item2.Food.ShouldBe(2);
+            Assert.Equal(2, fieldWithRabbit.Item2.Food);
         }
 
         [Fact]
-        void GivenNotEnoughFood_RabbitDie()
+        public void GivenNotEnoughFood_RabbitDie()
         {
-            List<Lettuce> lettuces = new List<Lettuce>(new[] { new Lettuce(0, 2) });
+            List<Lettuce> lettuces = new List<Lettuce> { new Lettuce(0, 2) };
 
-            List<Rabbit> rabbits = new List<Rabbit>(new[]
+            List<Rabbit> rabbits = new List<Rabbit>
             {
                 new Rabbit(0, 3),
-            });
+            };
 
             var fieldWithRabbit = rabbitField.GetNextTurn(lettuces, rabbits);
 
-            fieldWithRabbit.Item2.Count().ShouldBe(0);
+            Assert.Equal(0, fieldWithRabbit.Item2.Count());
         }
 
         [Fact]
-        void GivenALotOfFood_NewRabbitIsBorn()
+        public void GivenALotOfFood_NewRabbitIsBorn()
         {
-            List<Lettuce> lettuces = new List<Lettuce>(new[] { new Lettuce(0, 20) });
+            List<Lettuce> lettuces = new List<Lettuce> { new Lettuce(0, 20) };
 
-            List<Rabbit> rabbits = new List<Rabbit>(new[]
+            List<Rabbit> rabbits = new List<Rabbit>
             {
                 new Rabbit(0, 42),
-            });
+            };
 
             var fieldWithRabbit = rabbitField.GetNextTurn(lettuces, rabbits);
 
-            fieldWithRabbit.Item2.Count().ShouldBe(2);
+            Assert.Equal(2, fieldWithRabbit.Item2.Count());
         }
 
         [Fact]
-        void EveryTurnRabitUsesSomeFood()
+        public void EveryTurnRabitUsesSomeFood()
         {
-            List<Lettuce> lettuces = new List<Lettuce>(new[] { new Lettuce(0, 0) });
+            List<Lettuce> lettuces = new List<Lettuce> { new Lettuce(0, 0) };
 
-            List<Rabbit> rabbits = new List<Rabbit>(new[]
+            List<Rabbit> rabbits = new List<Rabbit>
             {
                 new Rabbit(0, 10),
-            });
+            };
 
             var fieldWithRabbit = rabbitField.GetNextTurn(lettuces, rabbits);
 
-            fieldWithRabbit.Item2.First().Food.ShouldBe(8);
+            Assert.Equal(8, fieldWithRabbit.Item2.First().Food);
         }
     }
 }
