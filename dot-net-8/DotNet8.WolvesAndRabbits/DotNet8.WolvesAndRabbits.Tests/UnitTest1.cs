63dbfa0271b90805e5c70830053fb9d1321f2cb3
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2024-05-18 09:23:55 +0200

    copy Rabbit and Wolf tests

--- dot-net-8/DotNet8.WolvesAndRabbits/DotNet8.WolvesAndRabbits.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace DotNet8.WolvesAndRabbits.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
---
+++ dot-net-8/DotNet8.WolvesAndRabbits/DotNet8.WolvesAndRabbits.Tests/WolfFieldTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using KMorcinek.WolvesAndRabbits.Configuration;
+using Xunit;
+using Xunit.Should;
+
+namespace KMorcinek.WolvesAndRabbits.Tests
+{
+    public class WolfFieldTests
+    {
+        readonly WolfField wolfField = new WolfField(new WolfFieldConfiguration
+        {
+            FoodConsumedForDinner = 2,
+        });
+
+        [Fact]
+        void GivenOneRabbitInWolfNeighborhood_WolfEatsWholeRabbit()
+        {
+            List<Rabbit> rabbits = new List<Rabbit>(new[] { new Rabbit(-20, 10) });
+
+            Wolf wolf = new Wolf(0, 10);
+            var fieldWithRabbit = wolfField.PredatorMovesAndEatsOnlyBestPrey(rabbits, wolf);
+
+            fieldWithRabbit.Item2.Food.ShouldBe(10 + 10);
+        }
+
+        [Fact]
+        void GivenOneRabbitInWolfNeighborhood_WolfKillsRabbit()
+        {
+            List<Rabbit> rabbits = new List<Rabbit>(new[] { new Rabbit(-20, 10) });
+
+            Wolf wolf = new Wolf(0, 10);
+            var fieldWithRabbit = wolfField.PredatorMovesAndEatsOnlyBestPrey(rabbits, wolf);
+
+            fieldWithRabbit.Item1.Count().ShouldBe(0);
+        }
+
+        [Fact]
+        void GivenRabbitsOutsideOfWolfNeighborhood_WolfDoesNotEat()
+        {
+            List<Rabbit> rabbits = new List<Rabbit>(new[] { new Rabbit(1, 10) });
+
+            Wolf wolf = new Wolf(-1, 10);
+            var fieldWithRabbit = wolfField.PredatorMovesAndEatsOnlyBestPrey(rabbits, wolf);
+
+            fieldWithRabbit.Item2.Food.ShouldBe(10);
+        }
+
+        [Fact]
+        void GivenNoRabbits_WolfDoesNotEat()
+        {
+            IEnumerable<Rabbit> rabbits = Enumerable.Empty<Rabbit>();
+
+            Wolf wolf = new Wolf(0, 10);
+            var fieldWithRabbit = wolfField.PredatorMovesAndEatsOnlyBestPrey(rabbits, wolf);
+
+            fieldWithRabbit.Item2.Food.ShouldBe(10);
+        }
+
+        [Fact]
+        void GivenNoRabbits_WolfMovesDoesNotMove()
+        {
+            IEnumerable<Rabbit> rabbits = Enumerable.Empty<Rabbit>();
+
+            int wolfStartingPosition = 22;
+            Wolf wolf = new Wolf(wolfStartingPosition, 10);
+            var fieldWithRabbit = wolfField.PredatorMovesAndEatsOnlyBestPrey(rabbits, wolf);
+
+            fieldWithRabbit.Item2.Position.ShouldBe(wolfStartingPosition);
+        }
+    }
+}
