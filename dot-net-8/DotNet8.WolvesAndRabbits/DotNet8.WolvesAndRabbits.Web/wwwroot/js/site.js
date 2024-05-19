0abb3aae05052fc56c4b4d6b8b53fe18de7eb287
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2024-05-19 10:09:54 +0200

    do not ignore wwwroot

---
+++ dot-net-8/DotNet8.WolvesAndRabbits/DotNet8.WolvesAndRabbits.Web/wwwroot/js/site.js
@@ -0,0 +1,63 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+angular.module('app', [])
+    .config(function () {
+    });
+
+angular.module('app').controller('MainCtrl',
+    function ($scope) {
+        $(function () {
+            var fieldHub = $.connection.fieldHub;
+
+            var getNextTurns = function (leftTurns) {
+                leftTurns -= 1;
+                fieldHub.server.getNextTurn().done(function (nextTurn) {
+                    $scope.$apply(function() {
+                        $scope.data = nextTurn.cellArrays;
+                        $scope.iterationCount = nextTurn.iterationCount;
+                    });
+
+                    if (leftTurns === 0) {
+                        return;
+                    }
+
+                    getNextTurns(leftTurns);
+                });
+            };
+
+            var reset = function () {
+                fieldHub.server.reset(null).done(function (nextTurn) {
+                    $scope.$apply(function () {
+                        $scope.data = nextTurn.cellArrays;
+                        $scope.iterationCount = nextTurn.iterationCount;
+                    });
+                });
+            };
+
+            $.connection.hub.start()
+                .done(function () {
+                    reset();
+
+                    fieldHub.server.getConfiguration().done(function (configuration) {
+                        $scope.$apply(function() {
+                            $scope.configuration = configuration;
+                        });
+                    });
+                });
+
+            $scope.nextTurns = function (count) {
+                getNextTurns(count);
+            }
+
+            $scope.resetConfiguration = function () {
+                fieldHub.server.reset($scope.configuration).done(function (nextTurn) {
+                    $scope.$apply(function() {
+                        $scope.data = nextTurn.cellArrays;
+                        $scope.iterationCount = nextTurn.iterationCount;
+                    });
+                });
+            }
+        });
+    });
