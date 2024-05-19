e6e67e6e057fe41d626841531435484293a6fb59
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2024-05-19 10:22:48 +0200

    .

--- dot-net-8/DotNet8.WolvesAndRabbits/DotNet8.WolvesAndRabbits.Web/wwwroot/js/site.js
+++ dot-net-8/DotNet8.WolvesAndRabbits/DotNet8.WolvesAndRabbits.Web/wwwroot/js/site.js
@@ -1,30 +1,61 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+// $(document).ready(function() {
+//     $.ajax({
+//         url: '/api/next-game',
+//         type: 'GET',
+//         success: function(result) {
+//             console.log(result);
+//         },
+//         error: function(xhr, status, error) {
+//             console.error("Error: " + error);
+//         }
+//     });
+// });
+//
+
 // Write your JavaScript code.
 angular.module('app', [])
     .config(function () {
+        alert('ha')
     });
 
 angular.module('app').controller('MainCtrl',
     function ($scope) {
         $(function () {
-            var fieldHub = $.connection.fieldHub;
+            // var fieldHub = $.connection.fieldHub;
 
             var getNextTurns = function (leftTurns) {
                 leftTurns -= 1;
-                fieldHub.server.getNextTurn().done(function (nextTurn) {
-                    $scope.$apply(function() {
-                        $scope.data = nextTurn.cellArrays;
-                        $scope.iterationCount = nextTurn.iterationCount;
+
+                $(document).ready(function () {
+                    $.ajax({
+                        url: '/api/next-game',
+                        type: 'GET',
+                        success: function (result) {
+                            console.log(result);
+                            // $scope.$apply(function () {
+                            //     $scope.data = nextTurn.cellArrays;
+                            //     $scope.iterationCount = nextTurn.iterationCount;
+                            // });
+                            //
+                            // if (leftTurns === 0) {
+                            //     return;
+                            // }
+                            //
+                            // getNextTurns(leftTurns);
+
+                        },
+                        error: function (xhr, status, error) {
+                            console.error("Error: " + error);
+                            alert("Error: " + error);
+                        }
                     });
-
-                    if (leftTurns === 0) {
-                        return;
-                    }
-
-                    getNextTurns(leftTurns);
                 });
+
+                // fieldHub.server.getNextTurn().done(function (nextTurn) {
+                // });
             };
 
             var reset = function () {
@@ -36,16 +67,16 @@ angular.module('app').controller('MainCtrl',
                 });
             };
 
-            $.connection.hub.start()
-                .done(function () {
-                    reset();
-
-                    fieldHub.server.getConfiguration().done(function (configuration) {
-                        $scope.$apply(function() {
-                            $scope.configuration = configuration;
-                        });
-                    });
-                });
+            // $.connection.hub.start()
+            //     .done(function () {
+            //         reset();
+            //
+            //         fieldHub.server.getConfiguration().done(function (configuration) {
+            //             $scope.$apply(function() {
+            //                 $scope.configuration = configuration;
+            //             });
+            //         });
+            //     });
 
             $scope.nextTurns = function (count) {
                 getNextTurns(count);
@@ -53,7 +84,7 @@ angular.module('app').controller('MainCtrl',
 
             $scope.resetConfiguration = function () {
                 fieldHub.server.reset($scope.configuration).done(function (nextTurn) {
-                    $scope.$apply(function() {
+                    $scope.$apply(function () {
                         $scope.data = nextTurn.cellArrays;
                         $scope.iterationCount = nextTurn.iterationCount;
                     });
