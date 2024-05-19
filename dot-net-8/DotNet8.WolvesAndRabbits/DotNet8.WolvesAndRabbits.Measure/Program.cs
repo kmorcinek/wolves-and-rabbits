13f33e1e1aa3d8cfab8dfd9a4e48ec0146cfe025
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2024-05-19 15:47:13 +0200

    add Measure project

---
+++ dot-net-8/DotNet8.WolvesAndRabbits/DotNet8.WolvesAndRabbits.Measure/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using KMorcinek.WolvesAndRabbits.Configuration;
+using KMorcinek.WolvesAndRabbits.Utils;
+
+namespace KMorcinek.WolvesAndRabbits.Measure
+{
+    class Program
+    {
+        static void Main()
+        {
+            FullConfiguration configuration = FullConfiguration.CreateDefault();
+
+            IRandom systemRandom = new SystemRandom();
+            FieldManager fieldManager = new FieldManager(
+                new LettuceField(systemRandom, configuration.LettuceFieldConfiguration),
+                new RabbitField(configuration.RabbitFieldConfiguration),
+                new WolfField(configuration.WolfFieldConfiguration),
+                systemRandom);
+
+            Fields fields = fieldManager.Create();
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            const int repeatCount = 1000;
+            for (int i = 0; i < repeatCount; i++)
+            {
+                fields = fieldManager.GetNextTurn(fields);
+            }
+
+            TimeSpan timeSpan = stopwatch.Elapsed;
+
+            using (StreamWriter streamWriter = File.AppendText("elapsedTimes.txt"))
+            {
+                streamWriter.WriteLine(timeSpan);
+            }
+        }
+    }
+}
