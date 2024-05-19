88a6128b3b2a919b1535550c6350c0d3ca7a3da7
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2024-05-19 09:54:39 +0200

    next-game

---
+++ dot-net-8/DotNet8.WolvesAndRabbits/DotNet8.WolvesAndRabbits.Web/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.WolvesAndRabbits.Web.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class GameController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet("next-game")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+    }
+}
